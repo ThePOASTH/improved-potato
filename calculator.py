bc604cd4ea0b3e9e31807f12ffe69628b6ebacc1
Prathamesh Nale <prathameshnale007@gmail.com>
2026-08-17 16:23:52 +0530

    Add calculator starter template

---
+++ calculator.py
@@ -0,0 +1,55 @@
+#write ADD function
+
+
+
+#write SUBTRACT function
+
+
+
+#write MULTIPLICATION function
+
+
+
+#write DIVISION function
+
+
+
+#write REMAINDER function
+
+
+
+
+#Don't change anything below this line
+while True:
+    print("MENU")
+    print("Choose your option")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Remainder")
+    print("6.Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+        print(add(a,b))
+    elif choice == 2:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+        print(sub(a,b))
+    elif choice == 3:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+        print(multi(a,b))
+    elif choice == 4:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+        print(div(a,b))
+    elif choice == 5:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+        print(rem(a,b))
+    else:
+        print("Exiting.......")
+        break
